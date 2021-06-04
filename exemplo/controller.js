851bd5d76a10c4125830566147d748c1276667c7
Rafael Lins <rafael.lins777@gmail.com>
2021-06-04 18:54:35 -0300

    A&D Pronto

--- exemplo/controller.js
+++ exemplo/controller.js
@@ -1,8 +1,15 @@
 
+/**
+ * Resposta:
+ * 200 OK
+ * <envia a página login-sucesso.html>
+ */
 route.create("/login", (usuario, senha, lembrarSenha) => {
     // TODO Implementar corpo
+    // A sofia ficou de fazer esse
 })
 
 route.create("/esqueci-minha-senha", (email) => {
     // TODO Implementar corpo
+    // Maxwell ficou de fazer esse!
 })
