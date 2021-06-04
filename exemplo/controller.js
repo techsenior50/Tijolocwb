cd0075c55b6d4dc20ae3d0f5e7cf9ef912084641
Rafael Lins <rafael.lins777@gmail.com>
2021-06-04 18:48:39 -0300

    Inclusão do controller

---
+++ exemplo/controller.js
@@ -0,0 +1,8 @@
+
+route.create("/login", (usuario, senha, lembrarSenha) => {
+    // TODO Implementar corpo
+})
+
+route.create("/esqueci-minha-senha", (email) => {
+    // TODO Implementar corpo
+})
